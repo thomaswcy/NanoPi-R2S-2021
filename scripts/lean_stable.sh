9b6ac28b4003716118ea4f0531a19164a49f8ed5
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-05-09 20:12:34 +0800

    Update lean_stable.sh

--- scripts/lean_stable.sh
+++ scripts/lean_stable.sh
@@ -12,9 +12,6 @@ pushd package/community
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 
-# 超频到 1.6 GHz
-wget -P target/linux/rockchip/patches-5.4 https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/rockchip/patches-5.4/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
-
 # Add luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
